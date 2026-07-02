753aa9ecc563558dcbee169cd63d4490899526a7
Giulio Valente <valente95giulio@gmail.com>
2026-07-02 18:34:55 +0200

    po

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,7 @@
-import { useAppContext } from "./contexts/AppContext";
-import { Footer } from "./components/footer/Footer";
-import { Header } from "./components/header/Header";
-import { About } from "./components/sections/about/About";
-import { Contact } from "./components/sections/contact/Contact";
-import { HeroSection } from "./components/sections/hero/Hero";
-import { Projects } from "./components/sections/projects/Projects";
-import { AsciiElements } from "./components/Ascii_Elements/AnimatedPlayer.tsx/AsciiElements";
-import { useWaveData, WavyBackground } from "./components/wavy-background/Wavy_background";
-import { SlidingElement } from "./components/wavy-background/SlidingElement/SlidingElement";
-import { useOpacityAnimation } from "./shared/hooks/useOpacityAnimation";
-
 import "./style/fonts.css";
 
 function App() {
-  const { isDark, setIsDark } = useAppContext();
-  const wave = useWaveData();
-
-  const opacityAnim = useOpacityAnimation<HTMLDivElement>({
-    delay: 2000,
-    duration: 1000,
-    endOnScroll: true,
-  });
-
-  return (
-    <div className="App">
-      <AsciiElements />
-      <div className="opacity-layer" ref={opacityAnim.ref}>
-        {/* {!isMobile() && opacityAnim.hasStarted ? <CreativeCanvas /> : null} */}
-        <SlidingElement paths={[wave.paths[0], wave.paths[wave.paths.length - 1]]} />
-        <WavyBackground sections={wave.sections} totalHeight={wave.totalHeight} svgTop={wave.svgTop} padding={wave.padding} />
-        <Header setIsDark={setIsDark} isDark={isDark} />
-      </div>
-
-      <div className="page-content">
-        <HeroSection />
-        <Projects />
-        <About isDark={isDark} />
-        <Contact />
-      </div>
-      <Footer isDark={isDark} />
-    </div>
-  );
+  return <div className="app">A</div>;
 }
 
 export default App;
