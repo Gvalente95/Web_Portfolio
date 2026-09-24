571711664cacc0e36dcdc37a8f241094e964a36e
Giulio Valente <valente95giulio@gmail.com>
2026-09-25 01:36:38 +0200

    auto

--- src/components/sections/pages/home/hero/image/HeroImage.tsx
+++ src/components/sections/pages/home/hero/image/HeroImage.tsx
@@ -1,12 +1,34 @@
-import meIcon from "@/assets/png/photos/me.png";
+import icon0 from "@/assets/png/photos/0.png";
+import icon1 from "@/assets/png/photos/1.jpeg";
+import icon2 from "@/assets/png/photos/2.jpg";
+import icon3 from "@/assets/png/photos/3.jpg";
 
 import "./style.css";
+import { useState } from "react";
 
 export function HeroImage() {
+  const [index, setIndex] = useState(0);
+  const [changing, setChanging] = useState(false);
+  const icons = [icon0, icon1, icon2, icon3];
+
+  const nextImage = () => {
+    if (changing) return;
+
+    setChanging(true);
+
+    setTimeout(() => {
+      setIndex((prev) => (prev + 1) % icons.length);
+      setChanging(false);
+    }, 180);
+  };
+
   return (
     <div className={`hero-image`}>
-      <div className="image-container">
-        <img src={meIcon}></img>
+      <div className={`image-container ${changing ? "changing" : ""}`} onClick={nextImage}>
+        <img src={icons[index]} alt=""></img>
+        <div className="image-switch-hint">
+          <span>↻</span>
+        </div>
       </div>
     </div>
   );
